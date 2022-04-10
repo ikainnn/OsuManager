0b7860fc51096906f59d94a7b7b863b6c9637972
nyako <nyakonyns@gmail.com>
2022-04-10 01:01:30 -0300

    🎶

--- Include/KaedeManager/Manager.hpp
+++ Include/KaedeManager/Manager.hpp
@@ -1,8 +1,8 @@
 #ifndef KAEDE_MANAGER_HPP
 #define KAEDE_MANAGER_HPP
 
-#include "Core/Reader.hpp"
-#include "Core/Writer.hpp"
+#include "File/Reader.hpp"
+#include "File/Writer.hpp"
 #include "Collection.hpp"
 
 #endif
